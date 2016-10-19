28c1d03c2e103a354af4f9096c81cad7a26d24ce
kisquian <kisquian@gmail.com>
2016-10-19 10:16:54 -0300

    nodejs video2brain: Código del servidor

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+var servidor = require('http');
+
+function arrancaServidor(requiere, respuesta) {
+	respuesta.writeHead(200, {"Content-Type":"text/html"});
+	respuesta.write("<h1>The server is encaring the life</h1>");
+	respuesta.end();
+}
+
+servidior.createServer(arrancaServidor).listen(8888);
